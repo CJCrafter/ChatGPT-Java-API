3ff6dfa40d4e148d94585ebf2d968e0c81193e6c
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2023-03-17 06:56:52 +0100

    OkHttpsParameters creation

---
+++ src/main/java/OkHttpsParameters.java
@@ -0,0 +1,71 @@
+
+/**
+ * This class allows to define the HTTP parameters used by OkHttp to talk to
+ * OpenAI infrastructure.
+ * 
+ * @author Nicolas de Pomereu
+ *
+ */
+public class OkHttpsParameters {
+
+    public static final int OK_HTTP_READ_TIMEOUT_SECONDS = 300;
+    public static final int OK_HTTP_CONNECT_TIMEOUT_SECONDS = 300;
+    public static final int OK_HTTP_WRITE_TIMEOUT_SECONDS = 300;
+
+    private int OkHttpReadTimeoutSeconds = OK_HTTP_READ_TIMEOUT_SECONDS;
+    private int OkHttpConnecTimeoutSeconds = OK_HTTP_CONNECT_TIMEOUT_SECONDS;
+    private int OkHttpWriteTimeoutSeconds = OK_HTTP_WRITE_TIMEOUT_SECONDS;
+
+    /**
+     * Constructor
+     * 
+     * @param okHttpReadTimeoutSeconds   the HTTP read timeout in seconds - defaults
+     *                                   to 300 seconds
+     * @param okHttpConnecTimeoutSeconds the HTTP connect timeout in seconds -
+     *                                   defaults to 300 seconds
+     * @param okHttpWriteTimeoutSeconds  the HTTP connect timeout in seconds
+     *                                   defaults to 300 seconds
+     */
+
+    public OkHttpsParameters(int okHttpReadTimeoutSeconds, int okHttpConnecTimeoutSeconds,
+	    int okHttpWriteTimeoutSeconds) {
+	OkHttpReadTimeoutSeconds = okHttpReadTimeoutSeconds;
+	OkHttpConnecTimeoutSeconds = okHttpConnecTimeoutSeconds;
+	OkHttpWriteTimeoutSeconds = okHttpWriteTimeoutSeconds;
+    }
+
+    /**
+     * Returns the HTTP read timeout in seconds
+     * 
+     * @return the HTTP read timeout in seconds
+     */
+    public int getOkHttpReadTimeoutSeconds() {
+	return OkHttpReadTimeoutSeconds;
+    }
+
+    /**
+     * Returns the HTTP connect timeout in seconds
+     * 
+     * @return the HTTP connect timeout in seconds
+     */
+    public int getOkHttpConnecTimeoutSeconds() {
+	return OkHttpConnecTimeoutSeconds;
+    }
+
+    /**
+     * Returns the HTTP write timeout in seconds
+     * 
+     * @return the HTTP write timeout in seconds
+     */
+    public int getOkHttpWriteTimeoutSeconds() {
+	return OkHttpWriteTimeoutSeconds;
+    }
+
+    @Override
+    public String toString() {
+	return "OkHttpsParameters [OkHttpReadTimeoutSeconds=" + OkHttpReadTimeoutSeconds
+		+ ", OkHttpConnecTimeoutSeconds=" + OkHttpConnecTimeoutSeconds + ", OkHttpWriteTimeoutSeconds="
+		+ OkHttpWriteTimeoutSeconds + "]";
+    }
+
+}
