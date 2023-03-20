41b74f408f30332701c63cacbf4c2e4568497b3a
Collin <collinjbarber@gmail.com>
2023-03-20 10:48:55 -0400

    add to maven central repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import com.github.breadmoirai.githubreleaseplugin.GithubReleaseTask
 
-group = "me.cjcrafter"
+group = "com.cjcrafter"
 version = "1.0.0"
 
 plugins {
     `java-library`
     `maven-publish`
+    signing
     id("com.github.breadmoirai.github-release") version "2.4.1"
 }
 
@@ -37,16 +38,47 @@ tasks {
     }
 }
 
+// Create javadocJar and sourcesJar tasks
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.named("javadoc"))
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+// Signing artifacts
+signing {
+    isRequired = true
+    //useGpgCmd()
+
+    useInMemoryPgpKeys(
+        findProperty("SIGNING_KEY_ID").toString(),
+        findProperty("SIGNING_PRIVATE_KEY").toString(),
+        findProperty("SIGNING_PASSWORD").toString()
+    )
+    //sign(configurations["archives"])
+    sign(publishing.publications)
+}
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
 
+            artifact(javadocJar)
+            artifact(sourcesJar)
+
             pom {
                 name.set("OpenAI Java API")
                 description.set("Access OpenAI's API without the raw JSON/HTTPS requests")
                 url.set("https://github.com/CJCrafter/ChatGPT-Java-API")
+
+                groupId = "com.cjcrafter"
+                artifactId = "openai"
+
                 licenses {
                     license {
                         name.set("The MIT License")
@@ -73,8 +105,8 @@ publishing {
         maven {
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
             credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
+                username = findProperty("OSSRH_USERNAME").toString()
+                password = findProperty("OSSRH_PASSWORD").toString()
             }
         }
     }
@@ -84,7 +116,7 @@ tasks.register<GithubReleaseTask>("createGithubRelease").configure {
     // https://github.com/BreadMoirai/github-release-gradle-plugin
     owner.set("CJCrafter")
     repo.set("ChatGPT-Java-API")
-    authorization.set("Token ${findProperty("pass").toString()}")
+    authorization.set("Token ${findProperty("GITHUB_PASSWORD").toString()}")
     tagName.set(version.toString())
     targetCommitish.set("master")
     releaseName.set("v${version} BETA")
